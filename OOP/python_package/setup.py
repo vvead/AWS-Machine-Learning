3502f53037fe340934b5c7bd288eae8f3113016a
vvead <w.touhami@esi-sba.dz>
2021-10-11 00:46:59 +0200

    create a gaussian distribution python package

---
+++ OOP/python_package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['dsnd_distributions'],
+      author = 'Wided Ahlem Touhami',
+      author_email = 'w.touhami@esi-sba.dz',
+      zip_safe=False)
