21f7c62b8e53fda67dc50b69c55afcf4bcb50dfb
vvead <w.touhami@esi-sba.dz>
2021-10-11 11:35:07 +0200

    upload package to PyPi

--- OOP/python_package/setup.py
+++ OOP/python_package/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='dsnd_distributions',
-      version='1.2',
-      description='Gaussian distributions',
-      packages=['dsnd_distributions'],
+setup(name='probability_distributions_101',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_101'],
       author = 'Wided Ahlem Touhami',
       author_email = 'w.touhami@esi-sba.dz',
       zip_safe=False)
